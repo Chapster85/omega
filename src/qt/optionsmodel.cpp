f7c67b7e2751fb2e3b13c1ec51a2f330de9da2f1
Chapster85 <30816887+Chapster85@users.noreply.github.com>
2018-01-30 17:42:56 -0500

    Update optionsmodel.cpp
    Fixed case of:
    emit AnonymizeOmegaNetworkAmountChanged

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -355,7 +355,7 @@ bool OptionsModel::setData(const QModelIndex& index, const QVariant& value, int 
         case AnonymizeOmegaNetworkAmount:
             nAnonymizeOmegaNetworkAmount = value.toInt();
             settings.setValue("nAnonymizeOmegaNetworkAmount", nAnonymizeOmegaNetworkAmount);
-            emit anonymizeOmegaNetworkAmountChanged(nAnonymizeOmegaNetworkAmount);
+            emit AnonymizeOmegaNetworkAmountChanged(nAnonymizeOmegaNetworkAmount);
             break;
         case CoinControlFeatures:
             fCoinControlFeatures = value.toBool();
